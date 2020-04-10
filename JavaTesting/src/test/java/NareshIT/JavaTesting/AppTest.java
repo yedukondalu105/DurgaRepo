aed3866f6891e0d4da1cb9aaf20078d9de3e48ef
yedukondalu105 <uppalayk105@gmail.com>
2020-04-10 19:03:30 +0530

    Merging with Branch2

--- JavaTesting/src/test/java/NareshIT/JavaTesting/AppTest.java
+++ JavaTesting/src/test/java/NareshIT/JavaTesting/AppTest.java
@@ -18,7 +18,7 @@ public class AppTest
     public AppTest( String testName )
     {
         super( testName );
-        
+        System.out.println("TEst");
         //This is for pushing from Branch
         
     }
